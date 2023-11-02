96a484a803f7700b7187ae05ed4a95c835e79b0e
Ghant2906 <thaithang115@gmai.com>
2023-11-02 18:19:45 +0700

    resize img newRelease, update song source newRelease, change logo

--- src/services/songService.js
+++ src/services/songService.js
@@ -1,18 +1,27 @@
 import db from "../models/index"
 const axios = require('axios')
 
-let getSongNewRelease = () => {
-    return new Promise(async (resolve, reject) => {
+let getSongNewRelease = async () => {
         try {
             let data = await db.Song.findAll({
                 limit: 12,
                 order: [['createdAt', 'DESC']]
             })
-            resolve(data)
+            for (let i = 0; i < data.length; i++) {
+                try {
+                    let result = await axios.get(`https://mp3.zing.vn/xhr/media/get-source?type=audio&key=${data[i].keySource}`);
+                    let source = result.data.data.source['128']
+                    if(source !== data[i].source){
+                        await updateSource(data[i].id, source)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            return data
         } catch (error) {
-            reject(error)
+            console.log(error);
         }
-    })
 }
 
 let getTopSongs = async () => {
